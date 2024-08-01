9660b4915f62137007199421a84dba03e4ba4885
boss <smithsaruni16@gmail.com>
2024-08-01 10:12:42 +0300

    database fetch

---
+++ src/app/api/carts/route.js
@@ -0,0 +1,11 @@
+// pages/api/items.js
+import pool from "@/db";
+
+export default async function handler(req, res) {
+  try {
+    const { rows } = await pool.query("SELECT * FROM cart");
+    res.status(200).json(rows);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching cart" });
+  }
+}
